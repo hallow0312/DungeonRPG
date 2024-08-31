b0c9c44b9f0e6b7a46b695636ab503882a3d86a6
hallow0312 <leejaehyun0312@naver.com>
2024-08-31 23:53:35 +0900

    2024.08.31.사운드시스템

--- LOGlIKE/Assets/Scripts/Manager/ResourcesManager.cs
+++ LOGlIKE/Assets/Scripts/Manager/ResourcesManager.cs
@@ -2,18 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourcesManager :SingleTon<ResourcesManager>
+public class ResourcesManager : SingleTon<ResourcesManager>
 {
-    public T Load<T>(string path) where T : MonoBehaviour
+    public T Load<T>(string path) where T : Object // object : 시스템 오브젝트 
     {
         return Resources.Load<T>(path);
-        //는 Unity 프로젝트 내의 특정 경로에 있는 리소스를 로드하고
-        // 그 리소스가 MonoBehaviour를 상속하는 타입이라면 그 타입으로 반환해줌.
     }
-    public GameObject Create(string  path, Transform parent =null)
+
+    public GameObject Create(string path, Transform parent = null) //default 매개변수 
     {
         GameObject prefab = Resources.Load<GameObject>(path);
-        if(prefab == null)
+        if (prefab == null)
         {
             Debug.Log("Failed to Load Prefab");
             return null;
@@ -21,12 +20,13 @@ public class ResourcesManager :SingleTon<ResourcesManager>
         Instantiate(prefab, parent);
         return prefab;
     }
-    public void Release (GameObject prefab)
+    public void Release(GameObject prefab)
     {
-        if(prefab==null)
+        if (prefab == null)
         {
             return;
         }
+
         Destroy(prefab);
     }
 }
--- LOGlIKE/Assets/Scripts/Manager/SoundManager.cs
+++ LOGlIKE/Assets/Scripts/Manager/SoundManager.cs
@@ -1,8 +1,32 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SoundManager : MonoBehaviour
+public class SoundManager :SingleTon<SoundManager>
 {
-   
+    [SerializeField] AudioSource scenerySource;
+    [SerializeField] AudioSource effectSource;
+    string soundName;
+    public void Sound(AudioClip clip)
+    {
+        effectSource.PlayOneShot(clip);
+    }
+
+    public void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        soundName = scene.name;
+        scenerySource.clip = ResourcesManager.Instance.Load<AudioClip>(soundName);
+        scenerySource.loop = true;
+        scenerySource.Play();
+    }
+    public void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 }
